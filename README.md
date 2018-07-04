d467c12927bb497d86f1a71702228b96891eb776
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-07-03 23:16:19 -0400

    Added round increase funcitonality. Added sprites for a boss.

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ A simple puzzle RPG game
 * Bone vectors found on [Vecteezy](https://www.vecteezy.com/).
 * Spell art by [J. W. Bjerk (eleazzaar)](www.jwbjerk.com/art). Find this and other open art at: <http://opengameart.org>.
 * Skeleton enemy by [Jesse M.](https://jesse-m.itch.io/skeleton-pack)
+* Demon enemy by Luis Zuno aka [Ansimuz](https://ansimuz.itch.io/gothicvania-patreon-collection).
 * Background pattern from [Toptal Subtle Patterns](https://www.toptal.com/designers/subtlepatterns).
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,10 @@
-var newGameNext = true;
-var mode = 0;
+var gameState = 0;
 var aura = 0;
 var playerHP = 100;
 var roundCount = 1;
 var game = {};
 
 function newGame() {
-    var difficulty = 0;
     var bonesInRound = [];
     var bonesOnScreen = [];
     var spells = [];
@@ -16,39 +14,45 @@ function newGame() {
     var numberOfBoneTypes = 0;
     var numberOfBones = 0;
 
-    switch (difficulty) {
-        case 0:
+    switch (roundCount) {
+        case 1:
             numberOfBoneTypes = 2;
             numberOfBones = 9;
             break;
-        case 1:
+        case 2:
+            numberOfBoneTypes = 2;
+            numberOfBones = 10;
+            break;
+        case 3:
             numberOfBoneTypes = 3;
             numberOfBones = 10;
             break;
-        case 2:
-            numberofBoneTypes = 3;
-            numberOfBones = 12;
-            break;
         case 4:
-            numberOfBoneTypes = 4;
+            numberOfBoneTypes = 3;
             numberOfBones = 12;
             break;
         case 5:
-            numberOfBoneTypes = 4;
+            numberOfBoneTypes = 3;
             numberOfBones = 15;
             break;
-        case 5:
-            numberOfBoneTypes = 5;
+        case 6:
+            numberOfBoneTypes = 4;
             numbefOfBones = 15;
             break;
-        case 6:
-            numberOfBoneTypes = 5;
-            numberOfBones = 20;
-            break;
         case 7:
-            numberOfBoneTypes = 6;
-            numberOfBones = 20;
+            numberOfBoneTypes = 4;
+            numberOfBones = 18;
             break;
+        case 8:
+            numberOfBoneTypes = 5;
+            numberOfBones = 18;
+            break;
+        case 9:
+            numberOfBoneTypes = 5;
+            numberOfBones = 25;
+        case 10:
+            numberOfBoneTypes = 6;
+            numberOfBones = 30;
     }
 
     // Choose which bone images to use and their values.
@@ -60,11 +64,11 @@ function newGame() {
     for (var i = 0; i < numberOfBoneTypes; i++) {
         var boneId = boneIds[Math.floor(Math.random() * boneIds.length)];
         boneIds.splice(boneIds.indexOf(boneId), 1);
-        boneId = "assets/images/bone" + boneSet + boneId + ".png";
+        var image = "assets/images/bone" + boneSet + boneId + ".png";
         var rotation = Math.floor(Math.random() * 8) * 45;
         var boneAura = auras[Math.floor(Math.random() * auras.length)];
         auras.splice(auras.indexOf(boneAura), 1);
-        bonesInRound.push({ image: boneId, rotation, aura: boneAura });
+        bonesInRound.push({ image, rotation, aura: boneAura });
     }
 
     // Choose how many of each bone to display, and displays them.
@@ -73,6 +77,7 @@ function newGame() {
     var bonesHeader = $('<h1>');
     bonesHeader.text('Bones');
     $('#boneyard').append(bonesHeader);
+    $('#spellbook').empty();
 
     for (var i = 0; i < numberOfBones; i++) {
         var boneType = bonesInRound[Math.floor(Math.random() * bonesInRound.length)];
@@ -80,7 +85,7 @@ function newGame() {
         bone.attr('src', boneType.image);
         bone.css('transform', 'rotate(' + boneType.rotation + 'deg)');
         bone.css('float,left');
-        bone.css('margin','20px');
+        bone.css('margin', '20px');
         bone.attr('width', '125px');
         bone.attr('aura', boneType.aura);
         bone.on('click', function () {
@@ -99,17 +104,25 @@ function newGame() {
                     spell.css('border', 'solid 3px yellow');
                     spell.css('opacity', '1.0');
                 }
-                else{
+                else {
                     spell.css('border', 'solid 3px #0006');
                     spell.css('opacity', '0.5');
                 }
             }
 
             // Enemy attacks
-            if(game.enemy.enemyHP>0){
+            if (game.enemy.enemyHP > 0) {
                 playerHP -= game.enemy.power;
+                if (playerHP < 0) {
+                    playerHP = 0;
+                    // GAME OVER STATE
+
+                }
                 $('#playerHP').text(playerHP);
             }
+
+            // Check if there any any moves remaining.
+            setTimeout(noMoreMoves(), 1200);
 
         });
         $('#boneyard').append(bone);
@@ -117,11 +130,11 @@ function newGame() {
     }
 
     var emptyBone = $('<img>');
-    emptyBone.attr('src','assets/images/empty.png');
+    emptyBone.attr('src', 'assets/images/empty.png');
     emptyBone.css('float,left');
-    emptyBone.css('margin','20px');
-    emptyBone.attr('width','125px');
-    emptyBone.attr('height','125px');
+    emptyBone.css('margin', '20px');
+    emptyBone.attr('width', '125px');
+    emptyBone.attr('height', '125px');
     $('#boneyard').append(emptyBone);
 
     // Create the spells available in this round.
@@ -129,7 +142,7 @@ function newGame() {
     var spellAura = 0;
     var spellOrNot = 0;
     var stepsWithNoSpell = 0;
-    for (var i = 0; i < bonesOnScreen.length; ) {
+    for (var i = 0; i < bonesOnScreen.length;) {
         var index = Math.floor(Math.random() * bonesOnScreen.length);
         var bone = bonesOnScreen[index];
         bonesOnScreen.splice(index, 1);
@@ -139,20 +152,20 @@ function newGame() {
             var spellType = Math.floor(Math.random() * 2);
 
             //Final spell, check if there are no damage dealing spells yet
-            if(bonesOnScreen.length===0){
+            if (bonesOnScreen.length === 0) {
                 var hasDamageSpell = false;
                 for (var i = 0; i < spells.length; i++) {
                     var spell = spells[i];
-                    if(spell.attr('src').indexOf('fireball')!=-1){
+                    if (spell.attr('src').indexOf('fireball') != -1) {
                         hasDamageSpell = true;
                         break;
                     }
                 }
-                if(!hasDamageSpell){
+                if (!hasDamageSpell) {
                     spellType = 0;
                 }
             }
-            
+
             var spell = $('<img>');
             spell.attr('width', '100px');
             spell.attr('aura', spellAura);
@@ -174,23 +187,28 @@ function newGame() {
                 // Create the spell
                 spell.on('click', function () {
                     var auraToCast = parseInt($(this).attr('aura'));
-                    if(auraToCast===aura){
+                    if (auraToCast === aura) {
+                        if($('#enemyHP').text()==='0'){ 
+                            return; 
+                        }
                         alert("Dealt " + auraToCast + " damage");
                         game.enemy.enemyHP -= auraToCast;
                         $('#enemyHP').text(game.enemy.enemyHP);
                         $(this).fadeOut();
                         $('#spell-' + auraToCast).fadeOut();
-                        $('#enemy').attr('src','assets/images/Skeleton Hit.gif');
-                        if(game.enemy.enemyHP<=0){
+                        $('#enemy').attr('src', 'assets/images/Skeleton Hit.gif');
+                        if (game.enemy.enemyHP <= 0) {
                             $('#enemyHP').text('0');
                             $('#enemy').attr('src', 'assets/images/Skeleton Dead.gif');
                         }
-                        else{
-                            setTimeout(function(){
-                                $('#enemy').attr('src','assets/images/Skeleton Idle.gif');
+                        else {
+                            setTimeout(function () {
+                                $('#enemy').attr('src', 'assets/images/Skeleton Idle.gif');
                             }, 1000);
                         }
                     }
+                    // Check if there any any moves remaining.
+                    setTimeout(noMoreMoves(), 800);
                 });
             }
             //Healing spells
@@ -207,17 +225,22 @@ function newGame() {
                 // Create the spell
                 spell.on('click', function () {
                     var auraToCast = parseInt($(this).attr('aura'));
-                    if(auraToCast===aura){
+                    if (auraToCast === aura) {
                         alert("Healed " + auraToCast + " hit points");
                         playerHP += auraToCast;
                         $('#playerHP').text(playerHP);
                         $(this).fadeOut();
                         $('#spell-' + auraToCast).fadeOut();
                     }
-                    $('#enemy').attr('src','assets/images/Skeleton React.gif');
-                    setTimeout(function(){
-                        $('#enemy').attr('src','assets/images/Skeleton Idle.gif');
+                    setTimeout(noMoreMoves(), 800);
+                    if($('#enemyHP').text()==='0') { 
+                        return; 
+                    }
+                    $('#enemy').attr('src', 'assets/images/Skeleton React.gif');
+                    setTimeout(function () {
+                        $('#enemy').attr('src', 'assets/images/Skeleton Idle.gif');
                     }, 300);
+
                 });
             }
 
@@ -230,44 +253,44 @@ function newGame() {
             spellCol.append(spell);
             var spellAuraText = $('<p>');
             spellAuraText.text(spellAura);
-            spellAuraText.attr('id','spell-' + spellAura);
+            spellAuraText.attr('id', 'spell-' + spellAura);
             spellCol.append(spellAuraText);
             $('#spellbook').append(spellCol);
 
         }
     }
     var emptySpellCol = $('<div>');
-    emptySpellCol.css('float','left');
+    emptySpellCol.css('float', 'left');
     var emptySpell = $('<img>');
-    emptySpell.attr('src','assets/images/empty.png');
-    emptySpell.attr('width','140px');
-    emptySpell.attr('height','192px');
+    emptySpell.attr('src', 'assets/images/empty.png');
+    emptySpell.attr('width', '140px');
+    emptySpell.attr('height', '192px');
     emptySpellCol.append(emptySpell);
     emptySpellCol.append($('<p>'));
     $('#spellbook').append(emptySpell);
 
     // Create enemy
-    var maxDmgDealt=0;
-    var maxHpHealed=0;
+    var maxDmgDealt = 0;
+    var maxHpHealed = 0;
     for (var i = 0; i < spells.length; i++) {
         var spell = spells[i];
-        if(spell.attr('src').indexOf('fireball')!=-1){
+        if (spell.attr('src').indexOf('fireball') != -1) {
             maxDmgDealt += parseInt(spell.attr('aura'));
         }
-        else if(spell.attr('src').indexOf('heal')!=-1){
+        else if (spell.attr('src').indexOf('heal') != -1) {
             maxHpHealed += parseInt(spell.attr('aura'));
         }
     }
 
-    var enemyHP = Math.floor(Math.random()*maxDmgDealt);
+    var enemyHP = Math.floor(Math.random() * maxDmgDealt);
     $('#enemyHP').text(enemyHP);
-    var power = Math.floor(maxHpHealed/numberOfBones) + 1;
+    var power = Math.floor(maxHpHealed / numberOfBones) + 1;
     var enemyName = 'Skeleton';
     var sprite = 'assets/images/Skeleton Idle.gif'
 
     //Make skeleton's color match bones
     var enemyColor = 0;
-    switch(boneSet){
+    switch (boneSet) {
         case 1:
             enemyColor = 200;
             break;
@@ -279,25 +302,49 @@ function newGame() {
         case 4:
             enemyColor = 100;
             break;
-        default: 
+        default:
             console.log("Bone set is not recognized.")
     }
 
-    $('#enemy').attr('src',sprite);
-    $('#enemy').css('filter','hue-rotate(' + enemyColor + 'deg)');
+    $('#enemy').attr('src', sprite);
+    $('#enemy').css('filter', 'hue-rotate(' + enemyColor + 'deg)');
 
-    enemy = {enemyName, enemyHP, power, sprite, enemyColor};
+    enemy = { enemyName, enemyHP, power, sprite, enemyColor };
 
     // Display all info
+    aura = 0;
     $('#playerAura').text(aura);
     $('#playerHP').text(playerHP);
     $('#round').text(roundCount);
 
-    
-
-    return { difficulty, bonesInRound, spells, enemy};
+    return { bonesInRound, spells, enemy };
 }
 
-$('#clickMe').on('click',function(){
+function isEmpty(imgContainer) {
+    for (var i = 0; i < imgContainer.length; i++) {
+        var thisImg = imgContainer[i];
+        if (thisImg.getAttribute('src').indexOf('empty') >= 0) {
+            return true;
+        }
+        else if (thisImg.getAttribute('style').indexOf('none') === -1) {
+            return false;
+        }
+    }
+}
+
+function noMoreMoves(){
+    if(isEmpty($('#boneyard img'))){
+        if(game.enemy.enemyHP<=0){
+            console.log("YOU WIN");
+            roundCount++;
+            game = newGame();
+        }
+        else{
+            console.log("YOU LOSE");
+        }
+    }
+}
+
+$('#clickMe').on('click', function () {
     game = newGame();
 })
